b8f7a57b3dd398d6adfc242b5ac27f10a30145b9
Alain Bruno <alainbrunoofficial@gmail.com>
2025-04-30 22:32:09 +0100

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Create the socket connection
-const socket = io('http://localhost:5001', {
+const socket = io('https://ovs-backend-1.onrender.com', {
   withCredentials: true,
   transports: ['websocket', 'polling'],
 });
