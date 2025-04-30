aa01242c844ed5ab1d04345b5e0c635f84f420e4
Alain Bruno <alainbrunoofficial@gmail.com>
2025-04-30 23:01:56 +0100

    Update axiosInstance.js

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5001/api';
+const BASE_URL = 'https://ovs-backend-1.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL, 
