bf8fff12fc095242415b5a87487b74717c926e21
brunocode-s <alainbrunoofficial@gmail.com>
2025-06-19 00:42:54 +0100

    omo

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://ovs-backend-1.onrender.com';
+const BASE_URL = 'https://ovs-backend-1.onrender.com/api';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL, 
